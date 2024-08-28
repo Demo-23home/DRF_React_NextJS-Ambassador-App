42b6e36c45afcfd461b1e0ec2632fd10e29eaafd
demo-23home <zeyadslama23@gmail.com>
2024-08-28 12:50:03 +0300

    Adding Backend&Fronend APIViews for Proudct Model

--- ambassador/urls.py
+++ ambassador/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-
+from . import views
 
 
 urlpatterns = [
-    path("", include('common.urls'))
+    path("", include("common.urls")),
+    path("products/frontend/", views.ProductFrontendAPIView),
+    path("products/backend/", views.ProductBackendAPIView),
 ]
