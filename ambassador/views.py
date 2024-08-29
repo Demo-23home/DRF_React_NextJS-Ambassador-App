d08d4e0d16fa5a822188092ec967b850e9270619
demo-23home <zeyadslama23@gmail.com>
2024-08-29 14:47:27 +0300

    Adding Search Functionlity

--- ambassador/views.py
+++ ambassador/views.py
@@ -6,25 +6,33 @@ from .serializers import ProductSerializer
 from rest_framework.response import Response
 from django.core.cache import cache
 import time
+
+
 class ProductFrontendAPIView(APIView):
-    @method_decorator(cache_page(60*60*2,key_prefix='products_frontend'))
+    @method_decorator(cache_page(60 * 60 * 2, key_prefix="products_frontend"))
     def get(self, _):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
-    
-    
-    
-    
+
+
 class ProductBackendAPIView(APIView):
-    def get(self, _):
+    def get(self, request):
         products = cache.get("products_backend")
-        
-        if not products: 
+
+        if not products:
             time.sleep(4)
             products = list(Product.objects.all())
-            cache.set("products_backend", products, timeout=60*30) # 30 min
+            cache.set("products_backend", products, timeout=60 * 30)  # 30 min
+        search_query = request.query_params.get("search", "")
+        if search_query:
+            products = list(
+                [
+                    product
+                    for product in products
+                    if (search_query.lower() in product.title.lower())
+                    or (search_query.lower() in product.description.lower())
+                ]
+            )
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
-    
-    
