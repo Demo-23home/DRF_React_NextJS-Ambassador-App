04b14c830d2e547f4caddf5ce81fc0a1e837c97c
demo-23home <zeyadslama23@gmail.com>
2024-09-04 22:32:20 +0300

    Adding AtomicTransactions For Creating Order

--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,7 @@ from common.Authentication import JWTAuthentication
 from core.models import Link, Order, OrderItem, Product
 from rest_framework.response import Response
 from rest_framework import exceptions, status
+from django.db import transaction
 
 # Create your views here.
 
@@ -25,39 +26,46 @@ class OrderAPIView(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
+    @transaction.atomic
     def post(self, request):
-        data = request.data
-        code = data["code"]
-        link = Link.objects.filter(code=code).first()
-        user = request.user
-        products = data["products"]
+        try: 
+            data = request.data
+            code = data["code"]
+            link = Link.objects.filter(code=code).first()
+            user = request.user
+            products = data["products"]
 
-        if not link:
-            raise exceptions.APIException("Invalid Code!")
+            if not link:
+                raise exceptions.APIException("Invalid Code!")
 
-        order = Order()
-        order.code = link.code
-        order.user = user
-        order.ambassador_email = link.user.email
-        order.first_name = data["first_name"]
-        order.last_name = data["last_name"]
-        order.email = data["email"]
-        order.address = data["address"]
-        order.zip = data["zip"]
-        order.country = data["country"]
-        order.save()
+            order = Order()
+            order.code = link.code
+            order.user = user
+            order.ambassador_email = link.user.email
+            order.first_name = data["first_name"]
+            order.last_name = data["last_name"]
+            order.email = data["email"]
+            order.address = data["address"]
+            order.zip = data["zip"]
+            order.country = data["country"]
+            with transaction.atomic():
+                order.save()
 
-        for item in products:
-            product = Product.objects.get(id =item["product_id"])
-            quantity = decimal.Decimal(item["quantity"])
+            for item in products:
+                product = Product.objects.get(id=item["product_id"])
+                quantity = decimal.Decimal(item["quantity"])
 
-            order_item = OrderItem()
-            order_item.order = order
-            order_item.price = product.price
-            order_item.quantity = quantity
-            order_item.product_title = product.title
-            order_item.ambassador_revenue = decimal.Decimal(0.1) * product.price
-            order_item.admin_revenue = product.price * decimal.Decimal(0.9)
-            order_item.save()
-
+                order_item = OrderItem()
+                order_item.order = order
+                order_item.price = product.price
+                order_item.quantity = quantity
+                order_item.product_title = product.title
+                order_item.ambassador_revenue = decimal.Decimal(0.1) * product.price
+                order_item.admin_revenue = product.price * decimal.Decimal(0.9)
+                with transaction.atomic():
+                    order_item.save()
             return Response({"message": "success"})
+        except: 
+            transaction.rollback()
+            return Response("Error Occurred")
+
