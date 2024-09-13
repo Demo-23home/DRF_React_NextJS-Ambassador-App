fb6963a67318316a733a7de4181413bcef7e90b9
demo-23home <zeyadslama23@gmail.com>
2024-09-13 19:30:31 +0300

    Handling Reigster Logic

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -4,9 +4,29 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 
-class SuperUser(UserAdmin):
-    ordering = ["id"]
-    list_display = ("email", "is_ambassador", "is_staff", "first_name", "last_name")
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('email', 'first_name', 'last_name', 'is_ambassador', 'is_staff')
+    list_filter = ('is_ambassador', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ('user_permissions',)
 
-admin.site.register(User, SuperUser)
+admin.site.register(User, UserAdmin)
+
