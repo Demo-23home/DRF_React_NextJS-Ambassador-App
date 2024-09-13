5a3aff84217aa086b2d5a627dbafbfef857a353c
demo-23home <zeyadslama23@gmail.com>
2024-09-13 17:02:24 +0300

    Adding (Login, Register, Users) Components

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -1,48 +1,20 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Nav from './components/Nav';
-import Menu from './components/Menu'
+import Users from "./pages/Users";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Nav />
-        <div className="container-fluid">
-          <div className="row">
-            <Menu />
-            <main
-              role="main"
-              className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4"
-            >
-              <div className="table-responsive">
-                <table className="table table-striped table-sm">
-                  <thead>
-                    <tr>
-                      <th>#</th>
-                      <th>Header</th>
-                      <th>Header</th>
-                      <th>Header</th>
-                      <th>Header</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1,001</td>
-                      <td>Lorem</td>
-                      <td>ipsum</td>
-                      <td>dolor</td>
-                      <td>sit</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </main>
-          </div>
-        </div>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"  Component={Users} />
+        <Route path="/login" Component={Login} />
+        <Route path="/register" Component={Register} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
