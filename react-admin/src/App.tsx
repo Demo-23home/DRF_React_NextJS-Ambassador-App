cf8305fbf5f979a9532be24a2e62af5812707d21
demo-23home <zeyadslama23@gmail.com>
2024-09-13 16:25:17 +0300

    Adding React Components(Nav, Menu)

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -1,23 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from './components/Nav';
+import Menu from './components/Menu'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Nav />
+        <div className="container-fluid">
+          <div className="row">
+            <Menu />
+            <main
+              role="main"
+              className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4"
+            >
+              <div className="table-responsive">
+                <table className="table table-striped table-sm">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Header</th>
+                      <th>Header</th>
+                      <th>Header</th>
+                      <th>Header</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1,001</td>
+                      <td>Lorem</td>
+                      <td>ipsum</td>
+                      <td>dolor</td>
+                      <td>sit</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </main>
+          </div>
+        </div>
       </header>
     </div>
   );
