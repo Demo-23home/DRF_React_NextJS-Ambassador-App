95a4dd61e9def470672686a978edeb93d3e304c6
demo-23home <zeyadslama23@gmail.com>
2024-10-04 00:13:23 +0300

    Handling Search in Frontend in ProductsFrontend Endpoint

--- react-ambassador/src/pages/ProductsFrontend.tsx
+++ react-ambassador/src/pages/ProductsFrontend.tsx
@@ -2,30 +2,46 @@ import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import Products from "./Products";
 import { Product } from "../models/product";
-import axios from "axios";
+import axios, { all } from "axios";
 import { useNavigate } from "react-router-dom";
+import { setPriority } from "os";
 
 const ProductsFrontend = () => {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [allProducts, setAllProducts] = useState<Product[]>([]);
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
   const navigate = useNavigate();
   const [filters, setFilters] = useState({
-    s: ''
-  })
+    s: "",
+  });
 
   useEffect(() => {
     try {
       (async () => {
         const { data } = await axios.get("/products/frontend");
-        setProducts(data);
+        setAllProducts(data);
+        setFilteredProducts(data);
       })();
     } catch (e) {
       navigate("/login");
     }
   }, []);
 
+  useEffect(() => {
+    const products = allProducts.filter(
+      (p) =>
+        p.title.toLowerCase().indexOf(filters.s.toLocaleLowerCase()) >= 0 ||
+        p.description.toLowerCase().indexOf(filters.s.toLowerCase()) >= 0
+    );
+    setFilteredProducts(products);
+  }, [filters]);
+
   return (
     <Layout>
-      <Products products={products} filters={filters} setFilters={setFilters} />
+      <Products
+        products={filteredProducts}
+        filters={filters}
+        setFilters={setFilters}
+      />
     </Layout>
   );
 };
