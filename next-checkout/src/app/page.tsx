360bc81de60de97f8c61108a0c2ddfafbbd90bd1
demo-23home <zeyadslama23@gmail.com>
2024-10-04 22:16:59 +0300

    Adding Checkout Layout

--- next-checkout/src/app/page.tsx
+++ next-checkout/src/app/page.tsx
@@ -1,9 +1,131 @@
-import React from 'react'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 
-const page = () => {
+const CheckoutPage = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <div className="container">
+        <div className="py-5 text-center">
+          <h2>Welcome</h2>
+          <p className="lead">Has Invited You To Buy These Products</p>
+        </div>
 
-export default page
+        <div className="row">
+          <div className="col-md-4 order-md-2 mb-4">
+            <h4 className="d-flex justify-content-between align-items-center mb-3">
+              <span className="text-muted">Products</span>
+            </h4>
+            <ul className="list-group mb-3">
+              <li className="list-group-item d-flex justify-content-between lh-condensed">
+                <div>
+                  <h6 className="my-0">Product name</h6>
+                  <small className="text-muted">Brief description</small>
+                </div>
+                <span className="text-muted">$12</span>
+              </li>
+
+              <li className="list-group-item d-flex justify-content-between">
+                <span>Total (USD)</span>
+                <strong>$20</strong>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-8 order-md-1">
+            <h4 className="mb-3">Personal Info</h4>
+            <form className="needs-validation" noValidate>
+              <div className="row">
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="firstName">First name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="firstName"
+                    placeholder="First Name"
+                    required
+                  />
+                </div>
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="lastName">Last name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="lastName"
+                    placeholder="Last Name"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="email">
+                  Email <span className="text-muted">(Optional)</span>
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="you@example.com"
+                />
+                <div className="invalid-feedback">
+                  Please enter a valid email address for shipping updates.
+                </div>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="address">Address</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="address"
+                  placeholder="1234 Main St"
+                  required
+                />
+                <div className="invalid-feedback">
+                  Please enter your shipping address.
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-5 mb-3">
+                  <label htmlFor="country">Country</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="country"
+                    placeholder="Country"
+                    required
+                  />
+                </div>
+                <div className="col-md-4 mb-3">
+                  <label htmlFor="city">City</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="city"
+                    placeholder="City"
+                    required
+                  />
+                </div>
+                <div className="col-md-3 mb-3">
+                  <label htmlFor="zip">Zip</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="zip"
+                    placeholder="ZIP"
+                  />
+                </div>
+              </div>
+              <hr className="mb-20" />
+              <button className="btn btn-primary btn-lg w-100" type="submit">
+                Continue to checkout
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CheckoutPage;
